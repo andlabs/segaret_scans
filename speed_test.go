f3ca61a306afd055b9f8501d8f960727e9178af6
Pietro Gagliardi <pietro10@mac.com>
2012-08-29 19:37:04 -0400

    Added some benchmarks to test potential optimizations to the query for a list of consoles. Suggestion to use LIKE clause from Dantman in #mediawiki.

--- speed_test.go
+++ speed_test.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+
+	"strings"
+	"sort"
 )
 
 func BenchmarkSpeed(b *testing.B) {
@@ -22,7 +25,91 @@ func BenchmarkSpeed(b *testing.B) {
 	}
 }
 
-// console list
+func BenchmarkGetConsoleList(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _, err := sql_getconsoles()
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func _filter(s string) bool {
+	return !strings.HasPrefix(s, "19") &&			// omit years
+			!strings.HasPrefix(s, "20") &&
+			!strings.HasSuffix(s, " action") &&		// omit genres
+			!strings.HasSuffix(s, " adventure") &&	// TODO probably best to use a regexp
+			!strings.HasSuffix(s, " educational") &&
+			!strings.HasSuffix(s, " fighting") &&
+			!strings.HasSuffix(s, " puzzle") &&
+			!strings.HasSuffix(s, " racing") &&
+			!strings.HasSuffix(s, " shoot-'em-up") &&
+			!strings.HasSuffix(s, " shooting") &&
+			!strings.HasSuffix(s, " simulation") &&
+			!strings.HasSuffix(s, " sports") &&
+			!strings.HasSuffix(s, " table") &&
+			!strings.HasPrefix(s, "Unlicensed ") &&	// omit qualifiers
+			!strings.HasPrefix(s, "Unreleased ") &&
+			!strings.HasPrefix(s, "3D ") &&
+			!strings.HasPrefix(s, "Big box ") &&
+			!strings.HasPrefix(s, "US ") &&
+			!strings.HasPrefix(s, "EU ") &&
+			!strings.HasPrefix(s, "JP ") &&
+			!strings.HasPrefix(s, "Homebrew ") &&
+			!omitConsoles[s]
+}
+
+func BenchmarkGetConsoleList_Like(b *testing.B) {
+	b.StopTimer()
+	getconsoles, err := db.Prepare(
+//		`SELECT cat_title, cat_pages
+		`SELECT cat_title
+			FROM wiki_category
+			WHERE cat_title LIKE "%games"
+				AND cat_pages > 0;`)
+//			WHERE cat_title LIKE "%games";`)
+//			ORDER BY cat_title ASC;`)
+	if err != nil {
+		b.Fatalf("could not prepare console list query: %v", err)
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+	var consoles []string
+//	var nMembers []int32
+
+	res, err := getconsoles.Run()
+	if err != nil {
+		b.Fatalf("could not run console list query: %v", err)
+	}
+	gl, err := res.GetRows()
+	if err != nil {
+		b.Fatalf("could not get console list result rows: %v", err)
+	}
+	nameField := res.Map("cat_title")
+	if nameField < 0 {
+		b.Fatalf("could not locate console names: %v", err)
+	}
+//	countField := res.Map("cat_pages")
+//	if countField < 0 {
+//		b.Fatalf("could not locate console game count: %v", err)
+//	}
+	for _, v := range gl {
+		c := string(v[nameField].([]byte))
+//		if strings.HasSuffix(c, "_games") {
+			// make human readable and drop _games
+			c = strings.Replace(c, "_", " ", -1)
+			c = c[:len(c) - len(" games")]
+			if _filter(c) {
+				consoles = append(consoles, c)
+			}
+//			nMembers = append(nMembers, v[countField].(int32))
+//		}
+	}
+//	_ = sort.Strings
+	sort.Strings(consoles)
+	}
+}
+
 // game list
 
 const console = "Mega Drive"
