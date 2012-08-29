f392629eaf0de11fd0790b513b91a868b6d156b1
Pietro Gagliardi <pietro10@mac.com>
2012-08-29 10:18:24 -0400

    Add some database benchmarks for testing if I could optimize wikitext access.

--- speed_test.go
+++ speed_test.go
@@ -21,3 +21,129 @@ func BenchmarkSpeed(b *testing.B) {
 		getConsoleInfo(w, r)
 	}
 }
+
+// console list
+// game list
+
+const console = "Mega Drive"
+
+func BenchmarkGetWikitext(b *testing.B) {
+	b.StopTimer()
+	games, err := GetGameList(console)
+	if err != nil {
+		b.Fatalf("error getting %s game list: %v", console, err)
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		for _, game := range games {
+			_, err := sql_getwikitext(game)
+			if err != nil {
+				b.Fatalf("error retrieving game %s: %v", game, err)
+			}
+		}
+	}
+}
+
+func BenchmarkGetWikitext_N(b *testing.B) {
+	b.StopTimer()
+	getgames, err := db.Prepare(
+		`SELECT wiki_page.page_title
+			FROM wiki_page, wiki_categorylinks
+			WHERE wiki_categorylinks.cl_to = ?
+				AND wiki_page.page_id = wiki_categorylinks.cl_from
+				AND wiki_page.page_namespace = 0
+			ORDER BY wiki_page.page_title ASC;`)
+	if err != nil {
+		b.Fatalf("could not prepare game list query: %v", err)
+	}
+	getwikitext, err = db.Prepare(
+		`SELECT wiki_text.old_text
+			FROM wiki_page, wiki_revision, wiki_text
+			WHERE wiki_page.page_namespace = 0
+				AND wiki_page.page_title = ?
+				AND wiki_page.page_latest = wiki_revision.rev_id
+				AND wiki_revision.rev_text_id = wiki_text.old_id;`)
+	if err != nil {
+		b.Fatalf("could not prepare wikitext query (for scan list): %v", err)
+	}
+	var games [][]byte
+	category := "Mega_Drive_games"
+	res, err := getgames.Run(category)
+	if err != nil {
+		b.Fatalf("could not run game list query: %v", err)
+	}
+	gl, err := res.GetRows()
+	if err != nil {
+		b.Fatalf("could not get game list result rows: %v", err)
+	}
+	for _, v := range gl {
+		games = append(games, v[0].([]byte))
+	}
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, game := range games {
+			res, err := getwikitext.Run(game)
+			if err != nil {
+				b.Fatalf("could not run wikitext query (for scan list): %v", err)
+			}
+			wt, err := res.GetRows()
+			if err != nil {
+				b.Fatalf("could not get wikitext result rows (for scan list): %v", err)
+			}
+			_ = string(wt[0][0].([]byte))
+		}
+	}
+}
+
+func BenchmarkGetWikitext_ID(b *testing.B) {
+	b.StopTimer()
+	getgames, err := db.Prepare(
+		`SELECT wiki_page.page_id
+			FROM wiki_page, wiki_categorylinks
+			WHERE wiki_categorylinks.cl_to = ?
+				AND wiki_page.page_id = wiki_categorylinks.cl_from
+				AND wiki_page.page_namespace = 0
+			ORDER BY wiki_page.page_title ASC;`)
+	if err != nil {
+		b.Fatalf("could not prepare game list query: %v", err)
+	}
+	getwikitext, err = db.Prepare(
+		`SELECT wiki_text.old_text
+			FROM wiki_page, wiki_revision, wiki_text
+			WHERE wiki_page.page_namespace = 0
+				AND wiki_page.page_id = ?
+				AND wiki_page.page_latest = wiki_revision.rev_id
+				AND wiki_revision.rev_text_id = wiki_text.old_id;`)
+	if err != nil {
+		b.Fatalf("could not prepare wikitext query (for scan list): %v", err)
+	}
+	var games []uint32
+	category := "Mega_Drive_games"
+	res, err := getgames.Run(category)
+	if err != nil {
+		b.Fatalf("could not run game list query: %v", err)
+	}
+	gl, err := res.GetRows()
+	if err != nil {
+		b.Fatalf("could not get game list result rows: %v", err)
+	}
+	for _, v := range gl {
+		games = append(games, v[0].(uint32))
+	}
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, game := range games {
+			res, err := getwikitext.Run(game)
+			if err != nil {
+				b.Fatalf("could not run wikitext query (for scan list): %v", err)
+			}
+			wt, err := res.GetRows()
+			if err != nil {
+				b.Fatalf("could not get wikitext result rows (for scan list): %v", err)
+			}
+			_ = string(wt[0][0].([]byte))
+		}
+	}
+}
