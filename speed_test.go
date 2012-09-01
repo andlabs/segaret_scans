1d7b6a238e771543141402b6a2e778357e66466b
Pietro Gagliardi <pietro10@mac.com>
2012-08-31 23:47:32 -0400

    Added some console page templated/untemplated benchmarks to the speed test.

--- speed_test.go
+++ speed_test.go
@@ -9,6 +9,7 @@ import (
 
 	"strings"
 	"sort"
+	"fmt"
 )
 
 func BenchmarkSpeed(b *testing.B) {
@@ -234,4 +235,153 @@ func BenchmarkGetWikitext_ID(b *testing.B) {
 			_ = string(wt[0][0].([]byte))
 		}
 	}
+}
+
+func BenchmarkConsolePage_NewCode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		url, _ := url.Parse("http://andlabs.sonicretro.org/scans/Mega Drive")
+		generateConsoleReport(console,
+			httptest.NewRecorder(),
+			*url)
+	}
+}
+
+func BenchmarkConsolePage_OldCode(b *testing.B) {
+	b.StopTimer()
+
+var top = `<html>
+<head>
+	<title>Sega Retro Scan Information: %s</title>
+	<style type="text/css">
+	.Bad {
+		background-color: #888800;
+	}
+	.Missing {
+		background-color: #880000;
+	}
+	.Incomplete {
+		background-color: #888800;
+	}
+	.Good {
+		background-color: #008800;
+	}
+	.Error {
+		background-color: #000000;
+		color: #FFFFFF;
+	}
+	</style>
+</head>
+<body>
+	<h1>Sega Retro Scan Information: %s</h1>
+`
+
+var beginTable = `
+	<table>
+		<tr>
+			<th><a href="%s">Game</a></th>
+			<th><a href="%s">Region</a></th>
+			<th><a href="%s">Box</a></th>
+			<th><a href="%s">Media</a></th>
+		</tr>
+`
+
+var gameStart = `
+		<tr>
+			<td><a href="http://segaretro.org/%s">%s</a></td>
+`
+
+var gameEntry = `
+			<td>%s</td>
+			<td class=%v>%v</td>
+			<td class=%v>%v</td>
+		</tr>
+`
+
+var gameError = `
+			<td colspan=3 class=Error>Error: %v</td>
+		</tr>
+`
+
+var gameNoScans = `
+			<td colspan=3 class=Missing>No scans</td>
+		</tr>
+`
+
+var endTable = `
+	</table>
+`
+
+const filterRegionName = "region"
+const sortOrderName = "sort"
+var sortOrders = map[string]SortOrder{
+	"region":		SortByRegion,
+	"box":		SortByBoxState,
+	"media":		SortByMediaState,
+}
+
+urlSort := func(url url.URL, order string) string {
+	q := url.Query()
+	q.Del("sort")
+	q.Add("sort", order)
+	url.RawQuery = q.Encode()
+	return url.String()
+}
+
+urlNoSort := func (url url.URL) string {
+	q := url.Query()
+	q.Del("sort")
+	url.RawQuery = q.Encode()
+	return url.String()
+}
+
+b.StartTimer()
+for i := 0; i < b.N; i++ {
+_url, _ := url.Parse("http://andlabs.sonicretro.org/scans/Mega Drive")
+url := *_url
+w := httptest.NewRecorder()
+	var filterRegion string
+
+	fmt.Fprintf(w, top, console, console)
+	scans, err := GetConsoleScans(console)
+	if err != nil {
+		fmt.Fprintf(w, "<p>Error getting %s scan info: %v</p>\n", console, err)
+		return
+	}
+	query := url.Query()
+	if x, ok := query[filterRegionName]; ok && len(x) > 0 {	// filter by region if supplied
+		filterRegion = x[0]
+	}
+	if x, ok := query[sortOrderName]; ok && len(x) > 0 {		// sort differently if asked
+		if so, ok := sortOrders[x[0]]; ok {				// but only if we passed a valid sort order
+			scans.Sort(so)
+		}
+	}
+//	stats := scans.GetStats(filterRegion)
+//	fmt.Fprintf(w, "%s\n<br>", stats.HTML())
+	fmt.Fprintf(w, beginTable,
+		urlNoSort(url), urlSort(url, "region"),
+		urlSort(url, "box"), urlSort(url, "media"))
+	for _, scan := range scans {
+		if scan.Error != nil {
+			fmt.Fprintf(w, gameStart, scan.Name, scan.Name)
+			fmt.Fprintf(w, gameError, scan.Error)
+			continue
+		}
+		if scan.HasNoScans {
+			fmt.Fprintf(w, gameStart, scan.Name, scan.Name)
+			fmt.Fprintf(w, gameNoScans)
+			continue
+		}
+		if filterRegion != "" &&		// filter by region if supplied
+			!strings.HasPrefix(scan.Region, filterRegion) {
+			continue
+		}
+		fmt.Fprintf(w, gameStart, scan.Name, scan.Name)
+		fmt.Fprintf(w, gameEntry,
+			scan.Region,
+			scan.BoxState, scan.BoxState,
+			scan.MediaState, scan.MediaState)
+	}
+	fmt.Fprintf(w, endTable)
+}
 }
