8e9063181fd978929cbe09d5531e209bf4699f65
Pietro Gagliardi <pietro10@mac.com>
2012-09-02 22:56:33 -0400

    Added a benchmark for all files we're checking on Sega Retro, rather than just one of them.

--- speed_test.go
+++ speed_test.go
@@ -17,6 +17,17 @@ func BenchmarkFileCheck(b *testing.B) {
 	}
 }
 
+func BenchmarkAllFiles(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, n := range filenames {
+			_, err := checkScanGood(n)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	}
+}
+
 func BenchmarkPageList(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, err := GetGameList(console)
